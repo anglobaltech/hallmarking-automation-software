4b656a732126f75a44c0c6edbb57150736917f50
anglobaltech <mqtltech@gmail.com>
2026-06-30 11:03:18 +0530

    vite added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white text-3xl p-10">
+      Tailwind is Working!
+    </div>
+  );
+}
+
